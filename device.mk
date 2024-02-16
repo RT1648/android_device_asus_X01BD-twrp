2fcfec831acc8c1811512a80faf6630e34f3a6d7
Rohit Tiwari <rohitttiwari52@gmail.com>
2024-02-16 20:12:26 +0530

    X01BD: treewide: cleanup & additions
    * Build Required Libs
    
    * Correct Partition Configuration In BoardConfig
    
    * Nuke Useless flags
    
    * Update All Files In The Tree
    
    * Properly Sort Out Flags In BoardConfig
    
    Signed-off-by: Rohit Tiwari <rohitttiwari52@gmail.com>

--- device.mk
+++ device.mk
@@ -1,11 +1,8 @@
 #
-# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2024 The Android Open Source Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-
-# Api
-PRODUCT_SHIPPING_API_LEVEL := 27
 
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
@@ -30,11 +27,15 @@ PRODUCT_PACKAGES += \
 
 # Take a few libraries from sources
 TARGET_RECOVERY_DEVICE_MODULES += \
+    android.hidl.base@1.0 \
+    libicuuc \
     libion \
     libxml2 \
     vendor.display.config@1.0
 
 RECOVERY_LIBRARY_SOURCE_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/android.hidl.base@1.0.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libicuuc.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so \
     $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@1.0.so
